87caba92dcfc7f4062b9c441167abfbbda9bd5fe
justinzzc <justin_zzc@163.com>
2016-08-19 14:02:58 +0800

    update readme and demo

--- demo/js/aniAuto.js
+++ demo/js/aniAuto.js
@@ -1,10 +1,91 @@
 (function ($) {
 
+    var aniNames = [
+        "bounce",
+        "flash",
+        "pulse",
+        "rubberBand",
+        "shake",
+        "headShake",
+        "swing",
+        "tada",
+        "wobble",
+        "jello",
+        "bounceIn",
+        "bounceInDown",
+        "bounceInLeft",
+        "bounceInRight",
+        "bounceInUp",
+        "bounceOut",
+        "bounceOutDown",
+        "bounceOutLeft",
+        "bounceOutRight",
+        "bounceOutUp",
+        "fadeIn",
+        "fadeInDown",
+        "fadeInDownBig",
+        "fadeInLeft",
+        "fadeInLeftBig",
+        "fadeInRight",
+        "fadeInRightBig",
+        "fadeInUp",
+        "fadeInUpBig",
+        "fadeOut",
+        "fadeOutDown",
+        "fadeOutDownBig",
+        "fadeOutLeft",
+        "fadeOutLeftBig",
+        "fadeOutRight",
+        "fadeOutRightBig",
+        "fadeOutUp",
+        "fadeOutUpBig",
+        "flipInX",
+        "flipInY",
+        "flipOutX",
+        "flipOutY",
+        "lightSpeedIn",
+        "lightSpeedOut",
+        "rotateIn",
+        "rotateInDownLeft",
+        "rotateInDownRight",
+        "rotateInUpLeft",
+        "rotateInUpRight",
+        "rotateOut",
+        "rotateOutDownLeft",
+        "rotateOutDownRight",
+        "rotateOutUpLeft",
+        "rotateOutUpRight",
+        "hinge",
+        "rollIn",
+        "rollOut",
+        "zoomIn",
+        "zoomInDown",
+        "zoomInLeft",
+        "zoomInRight",
+        "zoomInUp",
+        "zoomOut",
+        "zoomOutDown",
+        "zoomOutLeft",
+        "zoomOutRight",
+        "zoomOutUp",
+        "slideInDown",
+        "slideInLeft",
+        "slideInRight",
+        "slideInUp",
+        "slideOutDown",
+        "slideOutLeft",
+        "slideOutRight",
+        "slideOutUp",
+    ];
+
     function autoAni(item) {
         var delay = 0, duration = null;
         var $item = $(item);
+        $item.hide();
 
         function runItemAni() {
+            $item.show();
+
             //delay
             if ($item.attr('ani-delay')) {
                 delay = $item.attr('ani-delay');
@@ -40,8 +121,9 @@
 
             $item.removeClass('ani-auto');
 
+
             //scroll
-            if (undefined!=$item.attr('ani-scroll')) {
+            if (undefined != $item.attr('ani-scroll')) {
                 var offset = $item.attr('ani-scroll-offset');
 
                 if (offset) {
@@ -68,7 +150,6 @@
                 var top = $item.offset().top + offset;
 
 
-
                 $('body,html').animate({scrollTop: top}, delay || 1000);
 
                 //console.log('[' + $item.attr('id') + ']==>top:' + top);
@@ -79,11 +160,13 @@
         //trigger
         if ($item.attr('ani-trigger')) {
             var triggerSelector = $item.attr('ani-trigger');
-            $(triggerSelector).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
-                if (!$item.hasClass('animated')) {
-                    runItemAni();
-                }
-            });
+            $(triggerSelector).
+                one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+                function () {
+                    if (!$item.hasClass('animated')) {
+                        runItemAni();
+                    }
+                });
         } else {
             runItemAni();
         }
@@ -97,6 +180,8 @@
         var autoAniItems = $('.ani-auto', dom && $(dom));
         $.each(autoAniItems, function (index, item) {
             autoAni(item);
+
+
         });
     }
 
